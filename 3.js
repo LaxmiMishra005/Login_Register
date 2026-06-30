90d793e00f328c5e7aef782c7e5778d09e64a5f6
LaxmiMishra005 <mishrasweety005@gmail.com>
2026-06-30 22:23:04 +0530

    Initial commit

---
+++ 3.js
@@ -0,0 +1,67 @@
+const registerForm = document.getElementById("registerForm");
+
+if (registerForm) {
+
+    registerForm.addEventListener("submit", function(e) {
+
+        e.preventDefault();
+
+        const name = document.getElementById("name").value;
+        const email = document.getElementById("regEmail").value;
+        const password = document.getElementById("regPassword").value;
+        const confirmPassword = document.getElementById("confirmPassword").value;
+
+        if(name==="" || email==="" || password==="" || confirmPassword===""){
+            alert("Please fill all fields.");
+            return;
+        }
+
+        if(password !== confirmPassword){
+            alert("Passwords do not match.");
+            return;
+        }
+
+        localStorage.setItem("userName", name);
+        localStorage.setItem("userEmail", email);
+        localStorage.setItem("userPassword", password);
+
+        alert("Registration Successful!");
+
+        window.location.href="login.html";
+
+    });
+
+}
+
+
+// ================= LOGIN =================
+
+const loginForm = document.getElementById("loginForm");
+
+if(loginForm){
+
+    loginForm.addEventListener("submit", function(e){
+
+        e.preventDefault();
+
+        const email=document.getElementById("loginEmail").value;
+        const password=document.getElementById("loginPassword").value;
+
+        const savedEmail=localStorage.getItem("userEmail");
+        const savedPassword=localStorage.getItem("userPassword");
+
+        if(email===savedEmail && password===savedPassword){
+
+            alert("Login Successful!");
+
+            window.location.href="2.html";
+
+        }else{
+
+            alert("Invalid Email or Password!");
+
+        }
+
+    });
+
+}
